6a4304fed6155b33dae9d1fed6bbfa76de2ed0b4
macha <machaakkermans@gmail.com>
2022-01-13 23:30:49 +0100

    Alles staat correct in classes en alle grafieken staan in GUI

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from tkinter import messagebox
 import matplotlib.pyplot as plt
 import re
 import tkinter as Tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
+
 
 class gff_bestand():
     def __init__(self,bestandsnaam):
@@ -49,6 +49,7 @@ class gffb_bestand():
         self.bestand_opdelen()
         self.informatie_halen_uit_bestand2()
         self.dictonaries_aanmaken()
+        #self.sequentie_uit_bestand3()
 
     
     def bestand_opdelen(self):
@@ -107,10 +108,6 @@ class gffb_bestand():
                     per_kopje_in_bestand += regel
         per_gegeven.append(per_kopje_in_bestand)
         self.__lijst_met_gegevens.append(per_gegeven)
-        #    aantal += 1
-        #     if aantal == 50:
-        #         break
-
         del self.__lijst_met_gegevens[0]
 
     def dictonaries_aanmaken(self):
@@ -133,6 +130,21 @@ class gffb_bestand():
 
     def getdiconaries(self):
         return self.__gene, self.__cds, self.__rrna
+
+    def sequentie_uit_bestand3(self):
+        bestand = open(self.__bestand3_sequentie.name, "r")
+        self.__sequentie = ""
+        aantal = 1
+        for regel in bestand:
+            print(aantal)
+            for index in regel:
+                if index == "a" or index == "t" or index == "c" or index == "g":
+                    self.__sequentie += index
+            aantal += 1
+        bestand.close()
+
+    def getsequentie(self):
+        return self.__bestand2_informatie
 
 class Grafieken():
 
@@ -228,14 +240,16 @@ class Staafdiagram(Grafieken):
 
 
 class gui_maken():
-    def __init__(self,patronen,gff_totale_inhoud,gffb_cds):
+    def __init__(self,patronen,gff_totale_inhoud,gffb_cds,sequentie):
         self.__patronen = patronen
         self.__gff_totale_inhoud = gff_totale_inhoud
         self.__gffb_cds = gffb_cds
+        self.__sequentie = sequentie
         self.setgegevenstypes()
         self.setgegevenshypothetical_proteins()
         self.settabel()
         self.setgegevens_strands()
+        self.setgegevens_gc_percentage()
 
         self.gui_startscherm()
 
@@ -260,17 +274,28 @@ class gui_maken():
         for patroon in self.__patronen:
             self.__tabel.append(patroon)
 
+    def setgegevens_gc_percentage(self):
+        gc = round((self.__sequentie.count("c") + self.__sequentie.count("g")) / len(
+        self.__sequentie) *100, 2)
+        at = round((self.__sequentie.count("a") + self.__sequentie.count("t")) / len(
+        self.__sequentie)*100, 2)
+        self.__percentage = [gc, at]
+        self.__gc = self.__percentage[0] * 10
+        self.__at = self.__percentage[1] * 10
 
     def gui_startscherm(self):
         startscherm = Tk.Tk()
-        self.__knop = Tk.Button(heigh=2, master=startscherm, text="klik hier voor tabel patronen",command=self.tabel_patronen)
+        self.__knop = Tk.Button(heigh=2, master=startscherm, text="klik hier voor tabel met de gevonden patronen",command=self.tabel_patronen)
         self.__knop2 = Tk.Button(heigh=2, master=startscherm, text="klik hier voor grafiek met verschillende type ",command=self.grafiek_type)
         self.__knop3 = Tk.Button(heigh=2, master=startscherm,text="klik hier voor grafiek met hypotheticale proteins ",command=self.grafiek_hypothetical_proteins)
         self.__knop4 = Tk.Button(heigh=2, master=startscherm,text="klik hier voor grafiek met % +/- strands ",command=self.maak_grafiek_strands)
+        self.__knop5 = Tk.Button(heigh=2, master=startscherm,text="klik hier voor grafiek met GC en AT %  ",command=self.maak_grafiek_percentage)
+
         self.__knop.pack()
         self.__knop2.pack()
         self.__knop3.pack()
         self.__knop4.pack()
+        self.__knop5.pack()
         Tk.mainloop()
 
     def tabel_patronen(self):
@@ -343,6 +368,7 @@ class gui_maken():
         self.__grote[0] = (self.__grote[0]/totaal) *1000
         self.__grote[1] = (self.__grote[1] / totaal) * 1000
 
+
     def maak_grafiek_strands(self):
         grafiek_strands = Tk.Toplevel()
         upper_frame = Tk.Frame(grafiek_strands)
@@ -359,6 +385,28 @@ class gui_maken():
         pie.create_arc((2, 2, 152, 152), fill="blue",start= (360*self.__grote[0])/1000, extent=(360*self.__grote[1])/1000)
         quit_button = Tk.Button(heigh=2, width=10, text="return",
                                 command=grafiek_strands.destroy,
+                                master=bottom_frame)
+        quit_button.pack()
+
+    def maak_grafiek_percentage(self):
+        grafiek_percentage = Tk.Toplevel()
+        upper_frame = Tk.Frame(grafiek_percentage)
+        bottom_frame = Tk.Frame(grafiek_percentage)
+        upper_frame.pack()
+        bottom_frame.pack()
+        label = Tk.Label(upper_frame, text='percentage AT/GC %')
+        legenda = Tk.Label(bottom_frame, text=("legenda:\n -GC:",self.__percentage[0], "\n-AT: - ", self.__percentage[1]))
+        label.pack()
+        legenda.pack()
+        pie = Tk.Canvas(upper_frame, width=154, height=154)
+        pie.pack()
+        pie.create_arc((2, 2, 152, 152), fill="red", start=0,
+                       extent=(360 * self.__gc) / 1000)
+        pie.create_arc((2, 2, 152, 152), fill="blue",
+                       start=(360 * self.__gc) / 1000,
+                       extent=(360 * self.__at) / 1000)
+        quit_button = Tk.Button(heigh=2, width=10, text="return",
+                                command=grafiek_percentage.destroy,
                                 master=bottom_frame)
         quit_button.pack()
 
@@ -449,131 +497,32 @@ def grafieken(inhoud_gff,sequentie_genoom,cds):
         grafieken(inhoud_gff)
 
 
-def informatieinlezen_gff(bestandsnaam):
-    bestand = open(bestandsnaam)
-    inhoud_gff = []
-    for regel in bestand:
-        regel = regel.replace("\n","") # Haalt de enter eruit
-        lijst = regel.split("\t")      # splits de lijst op elke tab
-        inhoud_gff.append(lijst)
-    bestand.close()
-    return inhoud_gff
-
-
-def gbff_bestand_opdelen(bestandsnaam_gbff):
-    bestand1 = open("bestand1","w")
-    bestand2 = open("gene+cds.txt","w")
-    bestand3 = open("sequentie.txt","w")
-
-    bestand = open(bestandsnaam_gbff,"r")
-    regel_nummer = 0
-    for regel in bestand:
-        if regel_nummer >= 0 and regel_nummer <= 45:
-            bestand1.write(regel)
-        elif regel_nummer <= 49099 and regel_nummer >= 46:
-            bestand2.write(regel)
-        elif regel_nummer >= 49100:
-            bestand3.write(regel)
-        regel_nummer += 1
-    bestand1.close()
-    bestand2.close()
-    bestand3.close()
-    return bestand2, bestand3
-
-
-def sequentie_uit_bestand(sequentie_bestand):
-    bestand = open("sequentie.txt", "r")
+def sequentie_uit_bestand3(bestandnaam):
+    bestand = open(bestandnaam.name, "r")
     sequentie = ""
     for regel in bestand:
         for index in regel:
             if index == "a" or index == "t" or index == "c" or index == "g":
                 sequentie += index
+    bestand.close()
     return sequentie
-
-
-def informatie_uit_gene_cds_bestand(gene_cds_bestand):
-    """
-
-    :return: lijst_met_gene_cds - 2d lijst met inhoudt van bestand gene+cds.txt
-    - [["gene 517..1878", /gene="dnaA", ...] [CDS 517..1878, /gene="dnaA",...],
-      ["gene ..., n]]
-    """
-    bestand = open(gene_cds_bestand.name)
-    #bestand = open("test.txt")
-    lijst_met_gene_cds = []
-    per_cds_of_gene = []
-    per_kopje_in_bestand = ""
-    aantal = 0
-    for regel in bestand:
-        while regel.startswith(" "):  #Haalt alle spaties weg dat bij het
-            regel = regel[1:]         # begin van de regel staat
-        regel = regel.replace("\n","")
-        # zorgt ervoor dat elke keer wanneer "gene" of "CDS"staat er een nieuw
-        # lijst wordt aangemaakt en de oude wordt toegevoegt aan de 2d lijst
-        if regel.startswith("gene") or regel.startswith("CDS"):
-            per_cds_of_gene.append(per_kopje_in_bestand)
-            lijst_met_gene_cds.append(per_cds_of_gene)
-            per_cds_of_gene = []
-            per_cds_of_gene.append(regel)
-            per_kopje_in_bestand = ""
-        else:
-            # Zorgt ervoor dat kopjes die bestaan uit meerdere regels
-            # samen als een regel worden toegevoegt in een lijst
-            if regel.startswith("/"):
-                if per_kopje_in_bestand != "":
-                    per_cds_of_gene.append(per_kopje_in_bestand)
-                    per_kopje_in_bestand = ""
-                per_kopje_in_bestand += regel
-            else:
-                per_kopje_in_bestand += regel
-    per_cds_of_gene.append(per_kopje_in_bestand)
-    lijst_met_gene_cds.append(per_cds_of_gene)
-   #    aantal += 1
-   #     if aantal == 50:
-   #         break
-
-    del lijst_met_gene_cds[0]
-    #print (lijst_met_gene_cds)
-    return lijst_met_gene_cds
-
-
-def maken_dictonary(lijst_met_gene_cds):
-    aantal = 0
-    gene = {}
-    cds = {}
-    id_nummer = 1
-    for lijst in lijst_met_gene_cds:
-        if aantal%2 == 0:
-            gene[id_nummer] = lijst
-            #id_nummer += 1
-        else:
-            cds[id_nummer] = lijst
-            id_nummer += 1
-        aantal += 1
-    return gene, cds
 
 
 def main():
     bestandsnaam_gff = "gff_bestand.txt"
     bestandsnaam_gbff = "gbff.txt"
 
-    #inhoud_gff = informatieinlezen_gff(bestandsnaam_gff)
-    #gene_cds_bestand, sequentie_bestand = gbff_bestand_opdelen(bestandsnaam_gbff)
-
     gff = gff_bestand(bestandsnaam_gff)
     gff_cds, gff_gene, gff_trna, ggf_exon, gff_anders, gff_totale_inhoud = gff.getlijsten()
     gffb = gffb_bestand(bestandsnaam_gbff)
     gffb_gene, gffb_cds, gffb_rrna = gffb.getdiconaries()
 
-    #sequentie_genoom = sequentie_uit_bestand(sequentie_bestand)
-    #lijst_met_gene_cds = informatie_uit_gene_cds_bestand(gene_cds_bestand)
-    #gene, cds = maken_dictonary(lijst_met_gene_cds)
+    sequentie_bestand = gffb.getsequentie()
+    sequentie = sequentie_uit_bestand3(sequentie_bestand)
 
-    #grafieken(inhoud_gff,sequentie_genoom,cds)
-    #gevonden_patronen = patronen(cds)
     p = Patronen(gff_cds,gffb_gene,gffb_cds)
     patronen = p.getpatronen()
 
-    g = gui_maken(patronen,gff_totale_inhoud,gffb_cds)
+    g = gui_maken(patronen,gff_totale_inhoud,gffb_cds,sequentie)
 
 main()
